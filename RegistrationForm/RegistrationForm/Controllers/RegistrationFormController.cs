af07b5027a8dcb5ea0cdf64e977488bf91ae9818
Vin <winlqvin.it@gmail.com>
2015-04-08 08:18:18 +0700

    Add new project
    Add new project to my github

---
+++ RegistrationForm/RegistrationForm/Controllers/RegistrationFormController.cs
@@ -0,0 +1,48 @@
+﻿using RegistrationForm.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace RegistrationForm.Controllers
+{
+    public class RegistrationFormController : Controller
+    {
+        //
+        // GET: /RegistrationForm/
+
+        public ActionResult Index(RegistrationAccount g)
+        {
+
+            RegistrationAccount registrationaccount = new RegistrationAccount();
+            List<SelectListItem> sexs = new List<SelectListItem>();
+            
+            sexs.Add(new SelectListItem { Text = "Nam", Value = "0" });
+            sexs.Add(new SelectListItem { Text = "Nữ", Value = "1" });
+
+            
+            ViewBag.SexyType = sexs;
+            XmlDocument doc = new XmlDocument();
+            string pathfile = Server.MapPath("~/Data.xml");
+            doc.Load(pathfile);
+            XmlElement goc = doc.DocumentElement;
+            XmlNodeList listcity = goc.SelectNodes("TINH_THANH");
+            List<SelectListItem> cities = new List<SelectListItem>();
+            cities.Add(new SelectListItem { Text = "Tỉnh/Thành", Value = "0" });
+            int i = 1;
+            foreach (XmlNode item in listcity)
+            {
+                cities.Add(new SelectListItem { Text = item.Attributes["ten"].Value, Value = i.ToString() });
+                i++;
+            }
+            ViewBag.Citis = cities;
+
+            return View(g);
+        }
+
+    }
+}
---
+++ RegistrationForm/RegistrationForm/Models/Registration.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm.Models
+{
+    public class AccountInfo
+    {
+        private string _username;
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        private string _mail;
+
+        public string Mail
+        {
+            get { return _mail; }
+            set { _mail = value; }
+        }
+
+        private string _repassword;
+        public string RePassword
+        {
+            get { return _repassword; }
+            set { _repassword = value; }
+        }
+    }
+
+    public class PersonalInfo
+    {
+        private string _sex;
+
+        public string Sex
+        {
+            get { return _sex; }
+            set { _sex = value; }
+        }
+        private string _birthday;
+
+        public string Birthday
+        {
+            get { return _birthday; }
+            set { _birthday = value; }
+        }
+        private string _currentaddress;
+
+        public string Currentaddress
+        {
+            get { return _currentaddress; }
+            set { _currentaddress = value; }
+        }
+        private string _phonenumber;
+
+        public string Phonenumber
+        {
+            get { return _phonenumber; }
+            set { _phonenumber = value; }
+        }
+    }
+
+    public class RegistrationAccount
+    {
+        private AccountInfo _accountinfo;
+
+        public AccountInfo Accountinfo
+        {
+            get { return _accountinfo; }
+            set { _accountinfo = value; }
+        }
+        private PersonalInfo _personalInfo;
+
+        public PersonalInfo PersonalInfo
+        {
+            get { return _personalInfo; }
+            set { _personalInfo = value; }
+        }
+
+        public RegistrationAccount()
+        {
+            _accountinfo = new AccountInfo();
+            _personalInfo = new PersonalInfo();
+        }
+    }
+
+
+    public class SexyType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
